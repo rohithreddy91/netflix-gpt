8982c682c65c305e85bd40ba8e84be107e02fe3b
rohithreddy91 <Rohith@9151>
2025-07-21 11:59:53 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="text-lg font-bold text-green-700">
+      Hello Welcome to React!
+    </div>
+  );
+}
+
+export default App;
